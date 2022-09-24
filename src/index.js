7aca06f8ba8bf93bb2fd635d3277f395e3a827d6
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-09-24 15:27:38 -0700

    aaa

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@
 
 //*   IMPORTS  components  files
 import './css/common.css';
+import NewsApiService from './js/news-service';
 // import cardTemplate from './templates/oneCardTemplate.hbs';
 // import fetchImages from './js/news-service';
 // import { onClickLoadMoreBtn } from './js/load-more-btn';
@@ -22,9 +23,23 @@ const refs = {
   spinner: document.querySelector('.spinner-border'),
 };
  
-
-
+const newsApiService = new NewsApiService();
+console.log(NewsApiService);
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+function onSearch(e) {
+  e.preventDefault();
   
+  newsApiService.query = e.currentTarget.elements.query.value;
+  newsApiService.resetPage();
+  newsApiService.fetchArticles();
+}
+function onLoadMore() {
+  newsApiService.fetchArticles()
+}
+
+
+
 
 // import articlesTpl from './templates/articles.hbs';
 // import './css/common.css';
@@ -39,39 +54,31 @@ const refs = {
 //   selector: '[data-action="load-more"]',
 //   hidden: true,
 // });
-// const newsApiService = new NewsApiService();
 
-refs.searchForm.addEventListener('submit', onSearch);
-// loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
 
-function onSearch(e) {
-  e.preventDefault();
-  const API_KEY = '30114983-364137b9a9ec33f130a531f95';
-  const BASE_URL = 'https://pixabay.com/api/';
-  const searchQuery = e.currentTarget.searchQuery.value;
-  fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
-  )
-    .then(r => r.json())
-    .then(console.log);
-  // newsApiService.query = e.currentTarget.elements.query.value;
+
+
+
+  
+ 
 
   // if (newsApiService.query === '') {
   //   return alert('Введи что-то нормальное');
   // }
-}
+// }
 //   loadMoreBtn.show();
 //   newsApiService.resetPage();
 //   clearArticlesContainer();
 //   fetchArticles();
 // }
 
-// function fetchArticles() {
-//   loadMoreBtn.disable();
-//   newsApiService.fetchArticles().then(articles => {
-//     appendArticlesMarkup(articles);
-//     loadMoreBtn.enable();
-//   });
+// function onLoadMore() {
+//   newsApiService.fetchArticles()
+// //   loadMoreBtn.disable();
+// //   newsApiService.fetchArticles().then(articles => {
+// //     appendArticlesMarkup(articles);
+// //     loadMoreBtn.enable();
+// //   });
 // }
 
 // function appendArticlesMarkup(articles) {
--- src/js/news-service.js
+++ src/js/news-service.js
@@ -1,10 +1,3 @@
-
-
-
-
-
-
-
 // import axios from 'axios';
 
 // const API_KEY = '30114983-364137b9a9ec33f130a531f95';
@@ -27,8 +20,6 @@
 // }
 // export { getImgs };
 
-
-
 // export default async function fetchImages(value, page) {
 //   const url = 'https://pixabay.com/api/';
 //   const key = '11240134-58b8f655e9e0f8ae8b6e8e7de';
@@ -37,45 +28,46 @@
 //   return await axios.get(`${url}${filter}`).then(response => response.data);
 // }
 
-// const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
-// const BASE_URL = 'https://newsapi.org/v2';
-// const options = {
-//   headers: {
-//     Authorization: API_KEY,
-//   },
-// };
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
 
-// export default class NewsApiService {
-//   constructor() {
-//     this.searchQuery = '';
-//     this.page = 1;
-//   }
+  fetchArticles() {
+    const API_KEY = '30114983-364137b9a9ec33f130a531f95';
+    const BASE_URL = 'https://pixabay.com/api/';
+    const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
 
-//   fetchArticles() {
-//     const url = `${BASE_URL}/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
+    fetch(url)
+      .then(r => r.json())
+      .then(data => {
+        this.incrementPage();
+        // console.log(this.page);
+        // console.log(this);
+      });
 
-//     return fetch(url, options)
-//       .then(response => response.json())
-//       .then(({ articles }) => {
-//         this.incrementPage();
-//         return articles;
-//       });
-//   }
+    //     return fetch(url, options)
+    //       .then(response => response.json())
+    //       .then(({ articles }) => {
+    //         this.incrementPage();
+    //         return articles;
+    //       });
+  }
 
-//   incrementPage() {
-//     this.page += 1;
-//   }
+  incrementPage() {
+    this.page += 1;
+  }
 
-//   resetPage() {
-//     this.page = 1;
-//   }
+  resetPage() {
+    this.page = 1;
+  }
 
-//   get query() {
-//     return this.searchQuery;
-//   }
+  get query() {
+    return this.searchQuery;
+  }
 
-//   set query(newQuery) {
-//     this.searchQuery = newQuery;
-//   }
-// }
-
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
